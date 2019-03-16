e5020d7bc335c01e1022724aad9b9b194a1648de
sgohary <syednacer@gmail.com>
2019-03-16 16:40:23 -0400

    first commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,49 @@
+// Modules
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+// Components
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { BasePageComponent } from './partials/base-page/base-page.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
+// Services
+import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BasePageComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductsComponent,
+    ServicesComponent,
+    ContactComponent,
+    PageNotFoundComponent,
+    ContactListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    FlashMessagesModule
+  ],
+  providers: [FlashMessagesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
